de4f2ea6da3bdd8e7d8ace72291f584dfb3c85ee
Helewud <helewud@gmail.com>
2021-07-04 23:14:30 +0100

    mastering react

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
-import ReactMapGL, { NavigationControl, Marker } from "react-map-gl";
+import ReactMapGL, { NavigationControl, Marker, Popup } from "react-map-gl";
 import { listLogEntries } from "./API";
 
 const navControlStyle = {
@@ -10,6 +10,8 @@ const navControlStyle = {
 
 function App(props) {
   const [logEntries, setLogEntries] = useState([]);
+  const [showPopup, setShowPopup] = useState({});
+  const [addEntryLocation, setAddEntryLocation] = useState(null);
 
   const [viewport, setViewport] = useState({
     latitude: 8,
@@ -24,54 +26,97 @@ function App(props) {
     })();
   }, []);
 
-  //   const markers = React.useMemo(
-  //     () =>
-  //       logEntries.map((entry) => (
-  //         <Marker
-  //           key={entry._id}
-  //           longitude={entry.latitude}
-  //           latitude={entry.longitude}
-  //         >
-  //           <img src="../public/pin.png" alt="location marker" />
-  //           <p>{entry.title}</p>
-  //         </Marker>
-  //       )),
-  //     [props.logEntries]
-  //   );
+  const showAddMarkerPopup = (e) => {
+    const [longitude, latitude] = e.lngLat;
+    setAddEntryLocation({
+      longitude,
+      latitude,
+    });
+  };
 
   return (
     <ReactMapGL
       {...viewport}
       width="100vw"
       height="100vh"
+      doubleClickZoom={false}
       mapStyle="mapbox://styles/mapbox/streets-v11"
       onViewportChange={(nextViewport) => setViewport(nextViewport)}
+      onDblClick={showAddMarkerPopup}
     >
       <NavigationControl style={navControlStyle} />
       {logEntries.map((entry) => {
         return (
-          <Marker
-            key={entry._id}
-            latitude={entry.latitude}
-            longitude={entry.longitude}
-          >
-            <div>{entry.title}</div>
-            <svg
-              style={{
-                width: `calc(1vim * ${viewport.zoom})`,
-                height: `calc(1vim * ${viewport.zoom})`,
-              }}
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              stroke-width="1"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+          <>
+            <Marker
+              key={entry._id}
+              latitude={entry.latitude}
+              longitude={entry.longitude}
             >
-              <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
-              <circle cx="12" cy="10" r="3"></circle>
-            </svg>
-          </Marker>
+              <div
+                onClick={() =>
+                  setShowPopup({
+                    ...showPopup,
+                    [entry._id]: true,
+                  })
+                }
+              >
+                <svg
+                  style={{
+                    width: "24px",
+                    height: "24px",
+                    transform: "translate(-50%, -100% )",
+                  }}
+                  viewBox="0 0 24 24"
+                  stroke="black"
+                  stroke-width="1"
+                  fill="yellow"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+                  <circle cx="12" cy="10" r="3"></circle>
+                </svg>
+              </div>
+            </Marker>
+            {showPopup[entry._id]
+              ? showPopup && (
+                  <Popup
+                    latitude={entry.latitude}
+                    longitude={entry.longitude}
+                    closeButton={true}
+                    closeOnClick={true}
+                    onClose={() => setShowPopup({})}
+                    anchor="top"
+                  >
+                    <div className="popup">
+                      <h4>{entry.title}</h4>
+                      <p>{entry.comments}</p>
+                      <small>
+                        Visited on:{" "}
+                        {new Date(entry.visitDate).toLocaleDateString()}
+                      </small>
+                    </div>
+                  </Popup>
+                )
+              : null}
+            {addEntryLocation ? (
+              <>
+                <Popup
+                  latitude={addEntryLocation.latitude}
+                  longitude={addEntryLocation.longitude}
+                  //   closeButton={true}
+                  //   closeOnClick={true}
+                  //   onClose={() => setAddEntryLocation(null)}
+                  //   anchor="top"
+                >
+                  <div className="popup">
+                    <h4>Add your new log entry here</h4>
+                  </div>
+                </Popup>
+              </>
+            ) : null}
+          </>
         );
       })}
     </ReactMapGL>
